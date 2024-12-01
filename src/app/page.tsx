c09e59146fecbbd30e7b719b8c34aa3086a024cd
AdinayBN <adinaybaltabaeva2004@gmail.com>
2024-12-01 18:39:37 +0600

    Reinitialize repository

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import TodoistClone from "@/components/TodoistClone";
+import SignIn from "@/components/sign-in";
+
+export default function Home() {
+  return (
+    <div>
+      <TodoistClone />
+      <SignIn />
+    </div>
+  );
+}
