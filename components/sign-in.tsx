7244772bbc393e56e16f48ad6a1be3b40ad18653
AdinayBltbv <angelizada243@gmail.com>
2024-12-09 10:17:41 +0600

    upd sing-in

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -3,14 +3,17 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function SignIn() {
   const { data: session } = useSession();
+
   if (session) {
+    console.log(session);
     return (
       <>
-        Signed in as {session.user.email} <br />
+        Signed in as {session.user?.email || "No email available"} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
   }
+
   return (
     <>
       Not signed in <br />
