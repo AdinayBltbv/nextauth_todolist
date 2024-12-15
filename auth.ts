eca47420949b7ae90c29db236a8096a7738f989d
AdinayBltbv <angelizada243@gmail.com>
2024-12-15 17:22:35 +0600

    auth.ts,middleware.ts

--- auth.ts
+++ auth.ts
@@ -3,15 +3,8 @@ import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-<<<<<<< HEAD
-  providers: [Google],
-  pages: {
-    signIn: "/login",
-  },
-=======
   providers: [GitHub, Google],
 
->>>>>>> b943af9f9b1bb15fc7ee742d228d99e6d422c174
   callbacks: {
     authorized: async ({ auth }) => {
       return !!auth;
